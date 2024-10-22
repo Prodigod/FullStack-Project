141a73c3979b6de6d4ea4c710c4445bd01412766
Josh <flinkeydink@gmail.com>
2024-10-22 13:15:14 -0400

    app.jsx updated to ensure root structure.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Provider } from "react-redux";
+import store from "./store";
 
-function App() {
-  
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
-
-export default App
