ed0362b0dafc23301ed57d87abce0cf6a51f46b0
Josh <flinkeydink@gmail.com>
2024-10-22 14:21:45 -0400

    Logo added globally and extra styling for navbar.

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,14 +1,19 @@
 import { NavLink, Outlet } from "react-router-dom";
-
+import './layout.css'
 export default function Layout() {
   return (
     <>
       <header>
-        <nav>
+      <div className="banner">
+      <NavLink to="/">
+    <img src="/images/logo.jpg" alt="Banner"/>
+    </NavLink>
+    </div>
+      <nav className="navbar">
           <NavLink to="/">Home</NavLink>
-          <NavLink to="/Faculty"> Faculty</NavLink>
-          <NavLink to="/Departments"> Departments</NavLink>
-          <NavLink to="/Register"> Register</NavLink>
+          <NavLink to="/Faculty">Faculty</NavLink>
+          <NavLink to="/Departments">Departments</NavLink>
+          <NavLink to="/Register">Register</NavLink>
           <NavLink to="/Login"> Login</NavLink>
         </nav>
       </header>
