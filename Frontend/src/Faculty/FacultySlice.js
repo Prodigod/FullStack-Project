c26a5457ea722268ca3f137ea4df88e045403ded
Josh <flinkeydink@gmail.com>
2024-10-22 15:31:02 -0400

    FacultySlice complete, users may now get list of faculty, get individual faculty member, add a faculty member, and delete a faculty member from API.

---
+++ Frontend/src/Faculty/FacultySlice.js
@@ -0,0 +1,43 @@
+import api from "../store/api";
+
+const facultyApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    getFaculty: build.query({
+      query: () => "/Faculty",
+      transformResponse: (response) => response.data,
+      transformErrorResponse: (response) => response.data.error,
+      providesTags: ["Faculty"],
+    }),
+    getFacultyMember: build.query({
+      query: (id) => "/Faculty/" + id,
+      transformResponse: (response) => response.data,
+      transformErrorResponse: (response) => response.data.error,
+      providesTags: ["Faculty"],
+    }),
+    addFacultyMember: build.mutation({
+      query: (professor) => ({
+        url: "/Faculty",
+        method: "POST",
+        body: professor,
+      }),
+      transformResponse: (response) => response.data,
+      transformErrorResponse: (response) => response.data.error,
+      invalidatesTags: ["Faculty"],
+    }),
+    deleteFacultyMember: build.mutation({
+      query: (id) => ({
+        url: "/departments/" + id,
+        method: "DELETE",
+      }),
+      transformErrorResponse: (response) => response.data.error,
+      invalidatesTags: ["Faculty"],
+    }),
+  }),
+});
+
+export const {
+  useGetFacultyQuery,
+  useGetFacultyMemberQuery,
+  useAddFacultyMemberMutation,
+  useDeleteFacultyMemberMutation,
+} = departmentsApi;
